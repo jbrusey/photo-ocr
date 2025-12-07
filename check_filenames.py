0b0fae332b315614703ff60798115fcf2f001442
James Brusey <james.brusey@gmail.com>
2025-12-07 14:25:10 +0000

    use pandas

--- check_filenames.py
+++ check_filenames.py
@@ -1,16 +1,15 @@
-import csv
-import shutil
 from pathlib import Path
+
+import pandas as pd
 
 photo = Path("~/Downloads/images-to-delete").expanduser()
 csv_file = photo / "server_paths.csv"
 
-with open(csv_file) as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        p = row["server_path"]
-        clean_p = p.lstrip("/")
-        src = photo / clean_p
+df = pd.read_csv(csv_file)
 
-        if not src.exists():
-            print(f"Source path does not exist: {src}")
+for server_path in df["server_path"].astype(str):
+    clean_p = server_path.lstrip("/")
+    src = photo / clean_p
+
+    if not src.exists():
+        print(f"Source path does not exist: {src}")
